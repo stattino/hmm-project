3efe2839cbe5364f71383ded03558161f2fab16f
Oskar Stattin <stattin.o@gmail.com>
2017-01-17 20:50:12 +0100

    some tests for genC

--- sample.py
+++ sample.py
@@ -1,8 +1,19 @@
-from Graph import * 
+from Graph import *
+from hmm import *
+
 #some code to test it
 graph = Graph()
-G3 = graph.genEvenGraph(12, 0)
+G3 = graph.genEvenGraph(8, 0)
 G3 = graph.setSwitches(G3)
 #print(G3)
-[A, B] = graph.genSignals(G3)
+[A, B] = graph.genSignals(G3, 6)
 print(B)
+print(A)
+
+hmm = HMM()
+C = hmm.generateC(G3, B[1,], 0.05)
+print(C)
+
+#print(sum(C[:,0]))
+#print(sum(C[:,5]))
+#print(sum(C[0,:]))
