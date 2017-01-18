d225e3464f4acb842a19a0c322168b1f0f420c5b
Oskar Stattin <stattin.o@gmail.com>
2017-01-18 11:51:47 +0100

    genSignals remade

--- Graph.py
+++ Graph.py
@@ -1,8 +1,8 @@
 import numpy as np
 
+
 class Graph:
-    
-    def genEvenGraph(self, n, shuffle=0): #        (shuffle not yet completed.. leads to self loops)
+    def genEvenGraph(self, n, shuffle=0):  # (shuffle not yet completed.. leads to self loops)
         """
         Generates an even graph with degree 3 for every vertex.
         Maximum of one edge between 2 vertices. Returns a matrix.
@@ -15,73 +15,84 @@ class Graph:
             n - number of vertices
             shuffle - if 1, randomly shuffles graph.
       """
-        if (np.fmod(n,2)!=0):
+        if (np.fmod(n, 2) != 0):
             n += 1
         if (n < 5):
             n = 6
-        g = np.zeros((n,n))
-        for i in range(0,n):
+        g = np.zeros((n, n))
+        for i in range(0, n):
             u = np.random.choice([1, 2, 3], (1, 3), False)
-            if (i%2 == 0):
-                g[i, np.fmod(i+1, n)] = u[0,0]
-                g[i, np.fmod(i+2, n)] = u[0,1]
-                g[i, np.fmod(i-2, n)] = u[0,2]
+            if (i % 2 == 0):
+                g[i, np.fmod(i + 1, n)] = u[0, 0]
+                g[i, np.fmod(i + 2, n)] = u[0, 1]
+                g[i, np.fmod(i - 2, n)] = u[0, 2]
             else:
-                g[i, np.fmod(i+2, n)] = u[0,0]
-                g[i, np.fmod(i-2, n)] = u[0,1]
-                g[i, np.fmod(i-1, n)] = u[0,2]
+                g[i, np.fmod(i + 2, n)] = u[0, 0]
+                g[i, np.fmod(i - 2, n)] = u[0, 1]
+                g[i, np.fmod(i - 1, n)] = u[0, 2]
 
-        if (shuffle==1):
+        if (shuffle == 1):
             np.random.shuffle(g)
         return g
 
     def setSwitches(self, g):
         # Set switches for vertices at random. (L=2 and R=3)
-        for i in range(0,g.shape[0]):
-            g[i,i] = np.random.randint(2,4)
+        for i in range(0, g.shape[0]):
+            g[i, i] = np.random.randint(2, 4)
         return g
 
-    def genSignals(self, g, N = 100, p=0.05):
+    def genSignals(self, g, T=100, p=0.05):
         """Generates N observations from a train traversing the graph g.
             Rules?
         Input:
-          g - graph
-          N - number of observations
-          p - noise in observation
+          g - graph structure
+          T - number of observations desired
+          p - noise level in observation
         Returns:
-          true - trajectory made by the train
-          observed - trajectory observed (with noise)
+          true_path -   true trajectory made by the train.
+                        1st row: index of vertices
+                        2nd row: arrival labels
+                        3rd row: departure labels
+          observed - trajectory observed with noise from switches
+                        1st row: arrival labels
+                        2nd row: departure labels
       """
-        n = g.shape[0]
-        x_0 = np.random.randint(n)
-        true_obs = np.zeros((3, N))
-        observed = np.zeros((2, N))
-        true_obs[0,0] = x_0  #vertex
-        true_obs[1,0] = np.random.randint(1, 4) #edge
-        true_obs[2,0] = g[x_0, x_0] #switch
+        N = g.shape[0]
+        x_0 = np.random.randint(N)
+        true_path = np.zeros((3, T))
+        observed = np.zeros((2, T))
 
-        for i in range(0,N): #remake this to make it more clear
-            origin = true_obs[0, i]
-            edge = true_obs[1, i]
+        true_path[0, 0] = x_0  # vertex
+        true_path[1, 0] = 0  # not observed
+        true_path[2, 0] = np.random.randint(1, 3)
 
-            temp_g = np.copy(g[origin,])
-            temp_g[origin] = 0
-            dep_indx = np.nonzero(temp_g==edge)[0][0] #departure label of edge
-            arr_indx = g[dep_indx, origin] ##arrival label of edge
-            sw = g[dep_indx, dep_indx] 
+        # true_path[1, 0] = np.random.choice([1, true_path[2,0]], 1, False, [1/3, 2/3])
 
-            if i<N-1: # allow for a last loop just adding to the observed
-                true_obs[0, i+1] = dep_indx
-                true_obs[2, i+1] = sw
+        observed[0, 0] = 0 # By definition, doesnt observe this.
+        if true_path[2, 0] == 1: # noise only if L/R
+            observed[1, 0] = 1
+        else:
+            observed[1, 0] = np.random.choice([g[x_0, x_0], 2 if g[x_0, x_0] == 3 else 3], 1, False, [1 - p, p])
 
-            observed[0, i] = arr_indx
-            if arr_indx==1:
-                if i < N-1:
-                    true_obs[1, i+1] = sw
-                observed[1, i] = np.random.choice([sw, 2 if sw==3 else 3], 1, False, [p, 1-p])
+        for i in range(1, T):
+            x_prev = true_path[0, i - 1]
+            e_prev = true_path[2, i - 1]
+
+            temp_g = np.copy(g[x_prev,])
+            temp_g[x_prev] = 0
+            vertex_idx = np.nonzero(temp_g == e_prev)[0][0]  # index of next vertex
+            arr_label = g[vertex_idx, x_prev]  # arrival label of edge
+            sw = g[vertex_idx, vertex_idx]
+
+            true_path[0, i ] = vertex_idx
+            true_path[1, i ] = arr_label
+
+            observed[0, i] = arr_label
+            if arr_label == 1:
+                true_path[2, i] = sw
+                observed[1, i] = np.random.choice([sw, 2 if sw == 3 else 3], 1, False, [1 - p, p])
             else:
-                if i < N-1:
-                    true_obs[1, i+1] = 1
+                true_path[2, i] = 1
                 observed[1, i] = 1
 
-        return true_obs, observed
+        return true_path, observed
