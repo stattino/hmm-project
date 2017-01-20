85b86d3589eac1ac4cebaf2197175f71fa4020f9
Oskar Stattin <stattin.o@gmail.com>
2017-01-20 17:12:28 +0100

    some comments and style

--- Graph.py
+++ Graph.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 class Graph:
-
-    def genEvenGraph(self, n, shuffle=0):  # (shuffle not yet completed.. leads to self loops)
+    def genEvenGraph(self, n, shuffle=0):  # (shuffle not yet completed, it leads to self loops)
         """
         Generates an even graph with degree 3 for every vertex.
         Maximum of one edge between 2 vertices. Returns a matrix.
@@ -13,13 +12,17 @@ class Graph:
             3 - edge "R"
         Parameters:
             n - number of vertices
-            shuffle - if 1, randomly shuffles graph.
-      """
+            shuffle - if 1, randomly shuffles graph. (doesnt work)
+        """
         if (np.fmod(n, 2) != 0):
-            n += 1
+            raise ValueError('WrongInput')
         if (n < 5):
-            n = 6
+            raise ValueError('SmallInput')
+
         g = np.zeros((n, n))
+         # Heuristic, creates two circles of points, one outside and one inside.
+         # Each point is connected to its two neighbours on the same circle, and
+         # to the adjacent point in the inner circle.
         for i in range(0, n):
             u = np.random.choice([1, 2, 3], (1, 3), False)
             if (i % 2 == 0):
@@ -36,15 +39,13 @@ class Graph:
         return g
 
     def setSwitches(self, g):
-         # Set switches for vertices at random. (L=2 and R=3)
-         # These are the true switches.
+         # Set switches for vertices at random, L=2 and R=3.
         N = g.shape[0]
         sigmas = np.random.randint(2, 4, size=N)
         return sigmas
 
     def genSignals(self, g, sigmas, T=100, p=0.05):
         """Generates N observations from a train traversing the graph g.
-            Rules?
         Input:
           g - graph structure
           sigmas - switch
@@ -64,10 +65,9 @@ class Graph:
         true_path = np.zeros((3, T))
         observed = np.zeros((2, T))
 
-        true_path[0, 0] = x_0  # vertex
+        true_path[0, 0] = x_0
         true_path[1, 0] = 0  # not observed
         true_path[2, 0] = np.random.randint(1, 3)
-
         # true_path[1, 0] = np.random.choice([1, true_path[2,0]], 1, False, [1/3, 2/3])
 
         observed[0, 0] = 0 # By definition, doesnt observe this.
@@ -76,19 +76,18 @@ class Graph:
         else:
             observed[1, 0] = np.random.choice([sigmas[x_0], 2 if sigmas[x_0] == 3 else 3], 1, False, [1 - p, p])
 
+         # Deterministically traverse the graph to create the true path,  following the
+         # current switches, and adds noise to the observations.
         for i in range(1, T):
             x_prev = true_path[0, i - 1]
             e_prev = true_path[2, i - 1]
 
-            temp_g = np.copy(g[x_prev,])
-            temp_g[x_prev] = 0
-            vertex_idx = np.nonzero(temp_g == e_prev)[0][0]  # index of next vertex
-            arr_label = g[vertex_idx, x_prev]  # arrival label of edge
+            vertex_idx = np.nonzero(g[x_prev,] == e_prev)[0][0] # index of next vertex
+            arr_label = g[vertex_idx, x_prev]               # arrival label of edge
             sw = sigmas[vertex_idx]
 
             true_path[0, i ] = vertex_idx
             true_path[1, i ] = arr_label
-
             observed[0, i] = arr_label
             if arr_label == 1:
                 true_path[2, i] = sw
--- hmm.py
+++ hmm.py
@@ -9,10 +9,8 @@ class HMM():
 
     def genC(self, sigma):
         """
-        Creates the matrix C, where C[i,j] = c(s=s_i, t=j)
-        G = graph structure
-        Obs = [o_1, ..., o_T]
-        p = noise level on switch
+        Creates the matrix C, where C[i,j] = c(s=s_i, t=j) given the current
+        switch setting sigma.
         """
         T = self.Obs.shape[0]
         N = self.G.shape[0]
@@ -20,23 +18,17 @@ class HMM():
         C[:,0] = 1/(3*N)
         for j in range(1,T): # columns
             for i in range(0,N): # rows
-                 # Find indexes in graph for vertices from where 0/L/R comes.
-                 # Superfluous now that diagonal is zero
-                 #temp_g = np.copy(G[i,])
-                 #temp_g[i] = 0
-
-                # Index in G-matrix for the corresponding vertex
-                g_0 = np.nonzero(self.G[i,]==1)[0][0] #departure label of edge
+                # Departure label of edges.
+                g_0 = np.nonzero(self.G[i,]==1)[0][0]
                 g_L = np.nonzero(self.G[i,]==2)[0][0]
                 g_R = np.nonzero(self.G[i,]==3)[0][0]
 
-                # Index in C-matrix for the vertex and edge
+                # Index in C-matrix for the vertex and edge.
                 zero = 3*g_0 + self.G[g_0, i] - 1 # G[g_0, i] = 1, 2, 3 depending on 0/L/R
                 left = 3*g_L + self.G[g_L, i] - 1
                 right = 3*g_R + self.G[g_R, i] - 1
 
-                # switch setting of vertex
-                sw = sigma[i]
+                sw = sigma[i] # switch setting
 
                 # iteratively filling the matrix depending on switch settings and obs.
                 if self.Obs[j] == 1: # o_j = 0
@@ -67,11 +59,7 @@ class HMM():
 
     def genD(self, sigma):
         """
-        returns d, and d_index matrices.
-        G = graph structure
-        Obs = [o_1, ..., o_T]
-        p = noise level on switch
-
+        returns d, and d_index matrices given the switch setting sigma.
         d = probability of the most probable previous state given the observations and switches
         d_index = indexes of the most probable previous state given the observations and switches.
         key to the index:
@@ -86,12 +74,8 @@ class HMM():
 
         for j in range(1, T):  # columns
             for i in range(0, N):  # rows
-                 # Superfluous, sigmas separate, diagonal is zero
-                 # temp_g = np.copy(G[i,])
-                 # temp_g[i] = 0
-
                 # Index in G-matrix for the corresponding vertex
-                g_0 = np.nonzero(self.G[i,] == 1)[0][0]  # departure label of edge
+                g_0 = np.nonzero(self.G[i,] == 1)[0][0]
                 g_L = np.nonzero(self.G[i,] == 2)[0][0]
                 g_R = np.nonzero(self.G[i,] == 3)[0][0]
 
@@ -100,23 +84,22 @@ class HMM():
                 left = 3 * g_L + self.G[g_L, i] - 1
                 right = 3 * g_R + self.G[g_R, i] - 1
 
-                # switch setting of vertex
                 sw = sigma[i]
 
-                if self.Obs[j] == 1:  # o_j = 0
+                if self.Obs[j] == 1:
                     d[3 * i, j] = (1 - self.p) * np.maximum(d[right, j - 1], d[left, j - 1] )
                     d_index[3 * i, j] = np.where( d[right, j - 1] >= d[left, j - 1] , right, left)
-                    if sw == 2:  # sw = L
+                    if sw == 2:
                         d[3 * i + 1, j] = self.p * d[zero, j - 1]
                         d[3 * i + 2, j] = 0
                         d_index[3 * i +1, j] = zero
                         d_index[3 * i +2 , j] = np.NaN
-                    elif sw == 3:  # sw = R
+                    elif sw == 3:
                         d[3 * i + 1, j] = 0
                         d[3 * i + 2, j] = self.p * d[zero, j - 1]
                         d_index[3 * i +1, j] = np.NaN
                         d_index[3 * i +2 , j] = zero
-                elif self.Obs[j] == 2:  # o_j = L
+                elif self.Obs[j] == 2:
                     d[3 * i, j] = self.p * np.maximum(d[right, j - 1], d[left, j - 1])
                     d_index[3 * i, j] = np.where( d[right, j - 1] >= d[left, j - 1] , right, left)
                     if sw == 2:
@@ -129,7 +112,7 @@ class HMM():
                         d[3 * i + 2, j] = self.p * d[zero, j - 1]
                         d_index[3 * i +1, j] = np.NaN
                         d_index[3 * i +2 , j] = zero
-                elif self.Obs[j] == 3:  # o_j = R
+                elif self.Obs[j] == 3:
                     d[3 * i, j] = self.p * np.maximum(d[right, j - 1], d[left, j - 1])
                     d_index[3 * i, j] = np.where( d[right, j - 1] >= d[left, j - 1] , right, left)
                     if sw == 2:
@@ -144,30 +127,30 @@ class HMM():
                         d_index[3 * i +2 , j] = zero
         return d, d_index
 
- # Changes one random switch i, sigma_i, to a new setting.
-def newSigma(sigma):
-    N = sigma.shape[0]
-    change = np.random.randint(0,N)
-    new_sigma = sigma
-    new_sigma[change] = 2 if sigma[change]==3 else 3
+ # Changes one random switch i, sigma[i], to a new setting.
+def newSigma(sigma, proposal=0):
+    if proposal==0:
+        N = sigma.shape[0]
+        change = np.random.randint(0,N)
+        new_sigma = sigma
+        new_sigma[change] = 2 if sigma[change]==3 else 3
+    elif proposal==1:
+        new_sigma = genSigma(g)
     return new_sigma
 
-# return N sigmas
+# Rreturn a random set of sigmas.
 def genSigma(g):
-    # Set switches for vertices at random. (L=2 and R=3)
     N = g.shape[0]
     sigmas = np.random.randint(2, 4, size=N)
     return sigmas
 
 # MCMC - Metropolis hastings sampling of sigmas
-# Decide what the proposal distribution p(O | sigma)
-def sampleSigma(N, hmm, burn_in = 100, no_samples = 2000, skip_rate = 1):
-    sigmas = np.zeros((N, no_samples))      # Containing no_samples many combinations of sigma
-    sigma_probabilities = np.zeros( no_samples)  # Contains the probabilities of these sigmas.
-    sigma = genSigma(hmm.G)     # Randomize starting sigma
-
-     # Decorrelate samples by skipping skip_rate samples between each sample with skip_rate
-     # add later.
+# Decorrelate samples by skipping skip_rate samples between each sample with skip_rate
+# add later.
+def sampleSigma(N, hmm, burn_in=100, no_samples=2000, skip_rate=1):
+    sigmas = np.zeros((N, no_samples))
+    sigma_probabilities = np.zeros( no_samples)
+    sigma = genSigma(hmm.G)     # Random starting sigma
     for i in range(0, burn_in + no_samples):
         proposed_sigma = newSigma(sigma)
         prob_sigma = computeProbability(hmm, sigma)
@@ -185,7 +168,7 @@ def sampleSigma(N, hmm, burn_in = 100, no_samples = 2000, skip_rate = 1):
 
     return sigmas, sigma_probabilities
 
- # p( O |sigma, G )
+ # p(O| sigma, G)
 def computeProbability(hmm, sigma):
     C = hmm.genC(sigma)
     N, T = C.shape
@@ -193,18 +176,15 @@ def computeProbability(hmm, sigma):
     return prob
 
 
- # p(s | O, G)
+ # p(s| O, G)
 def computeTarget(hmm, sigmas):
     """
-    takes hmm, vector of sigmas
+    takes HMM, and its observations and graph structure, and a vector of sigmas
     returns p_vec = p(s| O, G)
     """
-    # p( O, s |sigma, G)
-
     prob_joint = np.zeros(3*np.sqrt(hmm.G.size))
     for row in sigmas:
         c = hmm.genC(row)
-        prob_joint += (c[:, -1])
-    prob_joint = np.divide(prob_joint, sum(prob_joint))
-
+        prob_joint += (c[:, -1])  # p(s, O| sigma, G)
+    prob_joint = np.divide(prob_joint, sum(prob_joint))     # Normalization
     return prob_joint
