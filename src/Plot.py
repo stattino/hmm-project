251ae6eef31e641a32b8a36427a3f5bb3c5c2137
Oskar Stattin <stattin.o@gmail.com>
2017-01-23 16:05:20 +0100

    ../plots

--- src/Plot.py
+++ src/Plot.py
@@ -20,22 +20,22 @@ class Plot:
         #plot probability (bar)
         data = pd.Series(p_vec)
         data.plot(kind="bar", color="k", alpha=0.7)
-        plt.savefig('./plots/bar/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
+        plt.savefig('../plots/bar/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
 
     def lineGraphObservation(self, p_matrix):
         df = pd.DataFrame(np.transpose(p_matrix))
         df.plot(grid=True, linewidth = 0.4)
-        plt.savefig('./plots/observation/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
+        plt.savefig('../plots/observation/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
 
     def lineGraphSample(self, p_matrix):
         y = np.log(p_matrix)
         df = pd.DataFrame(y)
         df.plot(grid=True, linewidth = 0.4, legend=False)
-        plt.savefig('./plots/samples/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
+        plt.savefig('../plots/samples/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
 
     def scatterObservation(self, p_matrix, true_states):
         plt.clf()
         for i in range(0, self.observation-1):
             plt.scatter(x=[i]*self.N*3, y=np.linspace(0, self.N*3-1, self.N*3), s=p_matrix[:, i]*250, color="blue")
             plt.scatter(x=i, y=true_states[i], s=200, facecolor="none", edgecolors="r")
-        plt.savefig('./plots/scatter/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
+        plt.savefig('../plots/scatter/nodes_{}-observation_{}-no_sample_{}_p={}.png'.format(self.N, self.observation, self.no_sample, self.p))
